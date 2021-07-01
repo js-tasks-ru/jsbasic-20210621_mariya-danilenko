f9e3ebc51f7fbd2dd4cd7dad84ea68f0ed536298
Maria danilenko <mariya.danienko@googlemail.com>
2021-07-01 17:29:05 +0300

    задачи к занятию один готовы.3 задача не полностью.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
-}
+    let result = 1;
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+    else
+    {while (n) {
+      result = result * n;
+      n = n - 1;
+    }}
+    return result;
+  }
+
+
--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,24 +1,32 @@
-/**
- * Эту функцию трогать не нужно
- */
-function print(text) {
-  console.log(text);
-}
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  // ваш код...
-}
-
-function sayHello() {
-  let userName = prompt('Введите ваше имя');
-
-  if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
-  } else {
-    print('Некорректное имя');
+  if ((name.includes (' ')) || (name.length <= 4) || (name == 0 ))
+  {
+    return false;
+  }
+  else
+  {
+    return true;
   }
 }
+
+
+//function allLetter(uname)
+// {
+//   var letters = /^[A-Za-z]+$/;
+//   if(uname.value.match(letters))
+//   {
+//     return true;
+//   }
+//   else
+//   {
+//     alert('Username must have alphabetcharactersonly');
+//     uname.focus();
+//     return false;
+//   }
+// }
+
+// если имя корректное, то true
+ //   × если содержит пробелы, то false
+ //   × если меньше 4 символов то false
+//
